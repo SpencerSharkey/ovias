ff3ea2b0013b96ef7a753fd65b129ddcb3ab9714
Slidefuse LLC <cloud9@sf-n.com>
2013-06-11 09:51:14 -0400

    re-added empty files oops, file IDE failed.

---
+++ gamemode/classes/buildmode/cl_buildmode.lua
@@ -0,0 +1,81 @@
+--[[
+    Ovias
+	Copyright © Slidefuse LLC - 2013
+--]]
+
+
+SF.BuildMode = {}
+
+function SF.BuildMode:StartBuild(type)
+	self.isBuilding = true
+	self.building = SF.Buildings:Get(type)
+	self.refreshBuilding = true
+end
+
+function SF.BuildMode:StopBuild()
+	self.isBuilding = false
+end
+
+function SF.BuildMode:Think()
+	if (self.isBuilding) then
+		if (!self.ghostEnt or self.refreshBuilding) then
+			self.ghostEnt = ClientsideModel(self.building:GetOviasModel(), RENDERGROUP_OTHER)
+			self.ghostEnt:SetModel(self.building:GetOviasModel())
+			local c = SF.Util:SetColorAlpha(SF.Client:GetFaction():GetColor(), 255*0.75)
+			self.ghostEnt:SetColor(c)
+			self.ghostEnt:SetNoDraw(true)
+			self.refreshBuilding = false
+		end
+
+		local trace = SF.Client:GetEyeTrace()
+
+		local req = self.building:GetRequirements()
+
+		self.canPlace, self.canPlaceError = req:Check(SF.Client:GetFaction(), trace, true)
+		self.placePos = trace.HitPos
+
+		self.ghostEnt:SetPos(trace.HitPos)
+		local angle = (SF.Client:GetPos() - trace.HitPos):Angle()
+		self.ghostEnt:SetAngles(angle)
+	else
+		if (self.ghostEnt) then
+			self.ghostEnt:Remove()
+			self.ghostEnt = nil
+		end
+	end
+end
+
+function SF.BuildMode:PlayerBindPress(ply, bind, pressed)
+	if (self.isBuilding) then 
+		if (bind == "+attack") then
+			netstream.Start("ovPlaceBuilding", self.building:GetTypeID())
+			return true 	
+		end
+	end
+end
+
+netstream.Hook("ovCancelBuildMode", function(data)
+	SF.BuildMode:StopBuild()
+end)
+
+function SF.BuildMode:PostDrawOpaqueRenderables()
+	if (self.isBuilding and self.ghostEnt and self.canPlace) then
+		render.SuppressEngineLighting(true)
+			local col = self.ghostEnt:GetColor()
+			render.SetBlend(0.75)
+			render.SetColorModulation(col.r/255, col.g/255, col.b/255, col.a/255)
+			self.ghostEnt:DrawModel()
+			render.SetBlend(1)
+		render.SuppressEngineLighting(false)
+	end
+end
+
+function SF.BuildMode:HUDPaint()
+	if (self.isBuilding and !self.canPlace and self.placePos) then
+		local pos = self.placePos:ToScreen()
+		surface.SetDrawColor(Color(255, 0, 0))
+		surface.DrawRect(pos.x-10, pos.y-10, 20, 20)
+	end
+end
+
+SF:RegisterClass("shBuilMode", SF.BuildMode)
---
+++ gamemode/classes/sh_faction.lua
@@ -0,0 +1,287 @@
+--[[
+    Ovias
+	Copyright © Slidefuse LLC - 2012
+--]]
+
+SF.Faction = {}
+SF.Faction.Colors = {
+	["Red"] = {Color(255, 0, 0), false},
+	["Pink"] = {Color(255, 0, 255), false},
+	["Purple"] = {Color(125, 0, 255), false},
+	["Blue"] = {Color(0, 0, 255), false},
+	["Light Blue"] = {Color(0, 255, 255), false}, 
+	["Green"] = {Color(0, 255, 0), false},
+	["Yellow"] = {Color(255, 255, 0), false},
+	["Orange"] = {Color(255, 125, 0), false}
+}
+SF.Faction.buffer = {}
+SF.Faction.netBuffer = {}
+
+SF.Faction.metaClass = {}
+SF.Faction.metaClass.__index = SF.Faction.metaClass
+
+function SF.Faction.metaClass:Init(keyO)
+	self.key = keyO or SF.Util:Random()
+	SF.Faction.netBuffer[self.key] = self
+
+	self.smartnet = SF.SmartNet:New(self.key)
+	self.smartnet:SetObject(self)
+
+	self.players = {}
+	self.territories = {}
+	self.buildings = {}
+	self.manors = {}
+	self.gold = 0
+
+	
+	if (SERVER) then
+		self.smartnet:AddObject("gold", 0)
+		self.smartnet:AddObject("players", {})
+		self.smartnet:AddObject("territories", {})
+		self.smartnet:AddObject("buildings", {})
+		self.smartnet:AddObject("color", Color(0, 0, 0))
+		self:AssociateColor()
+	end
+
+	if (CLIENT) then
+		self.territoryBuffer = {}
+		self.smartnet:AddCallback(function(data, faction)
+			if (data.gold) then
+				faction.gold = data.gold
+			end
+
+			if (data.player) then
+				faction.players = data.players
+			end
+
+			if (data.territories) then
+				faction.territoryBuffer = table.Merge(faction.territoryBuffer, data.territories)
+			end
+
+			if (data.buildings) then
+				faction.buildings = data.buildings
+			end
+
+			if (data.color) then
+				faction:SetColor(data.color)
+			end
+		end)
+	end
+
+end
+
+function SF.Faction.metaClass:UpdateTerritoryBuffer(territory)
+	local id = territory.index
+
+	for nKey, netID in next, self.territoryBuffer do
+		if (netID == id) then
+			self:AddTerritory(territory)
+			self.territoryBuffer[nKey] = nil
+		end
+	end
+
+end
+
+function SF.Faction.metaClass:GetNetKey()
+	return self.key
+end
+
+function SF.Faction.metaClass:Destroy()
+	self:DisassociateColor()
+end
+
+function SF.Faction.metaClass:GetBuildings()
+	return self.buildings
+end
+
+function SF.Faction.metaClass:GetBuildingsOfType(type)
+	if (!self.buildings[type]) then return {} end
+	return self.buildings[type]
+end
+
+function SF.Faction.metaClass:AddBuilding(building)
+	local btype = building:GetTypeID()
+	if (!self.buildings[btype]) then
+		self.buildings[btype] = {}
+	end
+
+	table.insert(self.buildings[btype], building)
+
+	if (SERVER) then
+		self.smartnet:UpdateObject("buildings", self.buildings, true)
+	end
+end
+
+function SF.Faction.metaClass:GetManors()
+	return self.manors
+end
+
+function SF.Faction.metaClass:GetGold()
+	return self.gold
+end
+
+function SF.Faction.metaClass:SetGold(gold)
+	self.gold = gold
+
+	if (SERVER) then
+		self.smartnet:UpdateObject("gold", self.gold, true)
+	end
+end
+
+function SF.Faction.metaClass:TakeGold(amount)
+	self.gold = self.gold - amount
+end
+
+function SF.Faction.metaClass:GiveGold(amount)
+	self.gold = self.gold + amount
+end
+
+function SF.Faction.metaClass:HasGold(amount)
+	return (self.gold >= amount)
+end
+
+function SF.Faction.metaClass:AssociateColor()
+	--Bruteforce the colors. Worst code in gamemode.
+	while (true) do
+		local tbl = table.Random(SF.Faction:GetColors())
+		local name = table.KeyFromValue(SF.Faction:GetColors(), tbl)
+		if (!tbl[2]) then
+			self:SetColor(name)
+			if (SERVER) then
+				self.smartnet:UpdateObject("color", name)
+			end
+			break
+		end
+	end
+end
+
+function SF.Faction.metaClass:DisassociateColor()
+	self:GetColorTable()[2] = false
+end
+
+function SF.Faction.metaClass:GetColorTable()
+	return SF.Faction.Colors[self:GetColorName()]
+end
+
+function SF.Faction.metaClass:GetColorName()
+	return self.colorName
+end
+
+function SF.Faction.metaClass:GetName()
+	return self:GetColorName().." Kingdom"
+end
+
+function SF.Faction.metaClass:GetColor()
+	return table.Copy(self.color)
+end
+
+function SF.Faction.metaClass:SetColor(name)
+	self.color = SF.Faction:GetColorFromName(name)
+	self.colorName = name
+end
+
+function SF.Faction.metaClass:AddPlayer(ply)
+	self.players[ply] = true
+	self:UpdateSmartnetPlayers()
+	self.smartnet:Transfer(ply)
+end
+
+function SF.Faction.metaClass:RemovePlayer(ply)
+	self.players[ply] = nil
+
+	if (table.Count(self.players) <= 0) then
+		SF:Msg("Faction has no players, destroying.")
+		self:Destroy()
+	else
+		self:UpdateSmartnetPlayers()
+	end
+end
+
+function SF.Faction.metaClass:UpdateSmartnetPlayers()
+	local plytable = {}
+	for ply in next, self.players do
+		table.insert(plytable, ply)
+	end
+	self.smartnet:SetPlayers(plytable)
+end
+
+function SF.Faction.metaClass:GetPlayers()
+	local ret = {}
+	for k in next, self.players do
+		table.insert(ret, k)
+	end
+	return ret
+end
+
+
+--Territory styff
+function SF.Faction.metaClass:AddTerritory(territory)
+	self.territories[territory.index] = territory
+
+	if (SERVER) then
+		local netTable = {}
+		for index in next, self.territories do
+			table.insert(netTable, index)
+		end
+
+		self.smartnet:UpdateObject("territories", netTable, true)
+	end
+end
+
+function SF.Faction.metaClass:RemoveTerritory(territory)
+	if (type(territory) == "number") then
+		self.territories[territory] = nil
+	else
+		self.territories[territory.index] = nil
+	end
+end
+
+function SF.Faction.metaClass:GetTerritory(index)
+	return self.territories[index]
+end
+
+function SF.Faction.metaClass:GetTerritories()
+	return self.territories
+end
+
+function SF.Faction.metaClass:PointInInfluence(point)
+	for _, territory in next, self.territories do
+		if (territory:PointInArea(point)) then
+			return true, territory
+		end
+	end
+	return false
+end
+
+/* End */
+
+function SF.Faction:OnTerritoryNetworked(territory)
+	--Assuming the territory was added to the faction... lets add it finally since we have the data :)
+	territory:GetFaction():UpdateTerritoryBuffer(territory)
+end
+
+function SF.Faction:Create(keyO)
+	local o = setmetatable({}, SF.Faction.metaClass)
+	table.insert(self.buffer, o)
+	o:Init(keyO)
+	SF:Call("OnFactionCreated", o)
+	return o
+end
+
+function SF.Faction:GetByNetKey(key)
+	return self.netBuffer[key]
+end
+
+function SF.Faction:GetFactions()
+	return self.buffer
+end
+
+function SF.Faction:GetColors()
+	return self.Colors
+end
+
+function SF.Faction:GetColorFromName(name)
+	return self.Colors[name][1]
+end
+
+SF:RegisterClass("shFaction", SF.Faction)
